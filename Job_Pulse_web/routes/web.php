81b585a7e14abb65dd65f60a407619adf46d3e8d
MAHMUDUL HASAN <128454891+Mhasan5505@users.noreply.github.com>
2024-03-20 22:51:18 +0600

    my commit

--- Job_Pulse_web/routes/web.php
+++ Job_Pulse_web/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\admin\DashboardController;
+use App\Http\Controllers\admin\JobApplicationController;
 use App\Http\Controllers\admin\JobController;
 use App\Http\Controllers\admin\UserController;
 use App\Http\Controllers\HomeController;
@@ -39,9 +40,11 @@ Route::group(['prefix' => 'admin','middleware' => 'checkRole'], function() {
     Route::put('/users/{id}', [UserController::class,'update'])->name('admin.users.update');
     Route::delete('/users', [UserController::class,'destroy'])->name('admin.users.destroy');
     Route::get('/jobs', [JobController::class,'index'])->name('admin.jobs');
-    Route::put('/jobs/edit/{id}', [JobController::class,'edit'])->name('admin.jobs.edit');
+    Route::get('/jobs/edit/{id}', [JobController::class,'edit'])->name('admin.jobs.edit');
     Route::put('/jobs/{id}', [JobController::class,'update'])->name('admin.jobs.update');
     Route::delete('/jobs', [JobController::class,'destroy'])->name('admin.jobs.destroy');
+    Route::get('/job-applications', [JobApplicationController::class,'index'])->name('admin.jobApplications');
+    Route::delete('/job-applications', [JobApplicationController::class,'destroy'])->name('admin.jobApplications.destroy');
 });
 
 Route::group(['account'], function() {
